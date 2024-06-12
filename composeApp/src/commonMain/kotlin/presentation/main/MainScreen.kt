34f13e946c933152e8ac4dcc80d3876fe6e20384
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2024-06-12 17:18:13 -0500

    implement multiplatform ui

--- composeApp/src/commonMain/kotlin/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/main/MainScreen.kt
@@ -1,8 +1,46 @@
 package presentation.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
 import data.koinViewModel
+import domain.model.Product
 
 @Composable
 fun MainScreen(navController: NavController) {
@@ -11,26 +49,114 @@ fun MainScreen(navController: NavController) {
 
 
 
+    AppContent(viewModel)
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppContent(viewModel: MainViewModel) {
+    val products = viewModel.products.value
 
+    BoxWithConstraints {
+        val maxWidth = this.maxWidth
+        var columns = 2
+        var modifier = Modifier.fillMaxWidth()
+        if (maxWidth > 840.dp) {
+            columns = 3
+         //   modifier = Modifier.width(maxWidth)
+            modifier = Modifier.widthIn(max= 1080.dp)
 
+        }
 
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(columns),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                //search bar
+                item(span = { GridItemSpan(columns) }) {
+                    Column {
+                        SearchBar(
+                            modifier = Modifier.fillMaxWidth(),
+                            query = "",
+                            active = false,
+                            onActiveChange = {},
+                            onQueryChange = {},
+                            onSearch = {},
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Search"
+                                )
+                            },
+                            placeholder = { Text("Search Products") }
+                        ) {}
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
 
+                }
 
+                items(products, key = {
+                    it.id.toString()
+                }) {
+                    ProductItem(it)
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun ProductItem(product: Product) {
+    Card(
+        shape = RoundedCornerShape(15.dp),
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(2.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            //compose image
+            AsyncImage(
+                modifier = Modifier.height(130.dp).padding(8.dp).align(Alignment.CenterHorizontally),
+                model = product.image,
+                contentDescription = product.title,
+                onError = {
+                    println("--------")
+                    println(it.result)
+                    println(it)
+                }
+            )
 
+            Text(
+                product.title.toString(),
+                textAlign = TextAlign.Start,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .heightIn(min = 40.dp)
+                    .padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-
-//        AsyncImage(
-//            modifier = Modifier
-//                .size(250.dp)
-//                .padding(16.dp).background(Color.Yellow),
-//            model = "https://media.themoviedb.org/t/p/w440_and_h660_face/czembW0Rk1Ke7lCJGahbOhdCuhV.jpg",
-//            contentDescription = null,
-//            onError = {
-//                println("--------")
-//                println(it.result)
-//                println(it)
-//            }
-//        )
+            Text(
+                "${product.price.toString()} USD ",
+                textAlign = TextAlign.Start,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.wrapContentWidth()
+                    .heightIn(min = 40.dp)
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
 }

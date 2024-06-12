9d90751910b5fe658018229efde755f06a6fbe18
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2024-06-12 15:14:39 -0500

    refactor

--- composeApp/src/commonMain/kotlin/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/main/MainScreen.kt
@@ -1,42 +1,36 @@
 package presentation.main
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import coil3.compose.AsyncImage
 import data.koinViewModel
 
 @Composable
 fun MainScreen(navController: NavController) {
-
     val viewModel = koinViewModel<MainViewModel>()
     val products = viewModel.products.value
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
 
-        AsyncImage(
-            modifier = Modifier
-                .size(250.dp)
-                .padding(16.dp).background(Color.Yellow),
-            model = "https://media.themoviedb.org/t/p/w440_and_h660_face/czembW0Rk1Ke7lCJGahbOhdCuhV.jpg",
-            contentDescription = null,
-            onError = {
-                println("--------")
-                println(it.result)
-                println(it)
-            }
-        )
-    }
+
+
+
+
+
+
+
+
+
+
+
+//        AsyncImage(
+//            modifier = Modifier
+//                .size(250.dp)
+//                .padding(16.dp).background(Color.Yellow),
+//            model = "https://media.themoviedb.org/t/p/w440_and_h660_face/czembW0Rk1Ke7lCJGahbOhdCuhV.jpg",
+//            contentDescription = null,
+//            onError = {
+//                println("--------")
+//                println(it.result)
+//                println(it)
+//            }
+//        )
 }
